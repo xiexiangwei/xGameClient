2a9f34ffdfbdd4cf1739a0428f3fc86d1ca688f8
xiexiangwei <xxw19900622@qq.com>
2017-05-14 02:11:50 +0800

    游戏客户端代码

---
+++ Assets/_Scripts/Const.cs
@@ -0,0 +1,32 @@
+﻿public static class Const
+{
+    //=======ErrorCode
+    public enum ERROR_CODE
+    {
+        ERROR_OK = 0,
+        ERROR_MAX_LOGINGATE = 1,
+        ERROR_NO_LOGINGATE = 2,
+        ERROR_NOT_READY_LOGIN = 3,
+    }
+
+    public enum CMD
+    {
+        //#=======心跳包
+        KEEPLIVE = 1,
+
+        //#客戶端--->管理服务器消息ID:[501-600]
+        C2SM_GET_LOGINGATE = 501,
+
+        //#管理服务器--->客戶端消息ID:[601-700]
+        SM2C_GET_LOGINGATE_REPLY = 601,
+
+        //#客户端--->登录网关消息ID:[701-800]
+        C2LG_Login = 701,
+
+        //#登录网关--->客户端消息ID:[801-900]
+        //LG2C_READY_TO_LOGIN=801,
+        LG2C_LOGIN_RESULT = 802,
+
+    }
+
+}
---
+++ Assets/_Scripts/Login.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperSocket.ClientEngine;
+using System.Net;
+using System;
+using ProtoBuf;
+using System.IO;
+
+public class Login : MonoBehaviour {
+
+    //管理服务器客户端
+    private static AsyncTcpSession smClient;
+    public void SendCmd(AsyncTcpSession client,Const.CMD cmd,byte[] data)
+    {
+        var lenArray = BitConverter.GetBytes(data.Length + 2);
+        var cmdArray = BitConverter.GetBytes((int)cmd);
+        byte[] head = new byte[] { lenArray[0], lenArray[1], cmdArray[0], cmdArray[1] };
+        var sendData = new byte[6 + data.Length];//6=包头(4)+包尾(2)
+        //包头
+        Buffer.BlockCopy(head, 0, sendData, 0, 4);
+        //包体
+        Buffer.BlockCopy(data, 0, sendData, 4 * sizeof(byte), data.Length);
+        //包尾
+        byte[] tail = new byte[] { 0x0, 0x0 };
+        Buffer.BlockCopy(tail, 0, sendData, (4 + data.Length) * sizeof(byte), 2);
+
+        client.Send(sendData, 0, sendData.Length);
+    }
+    void Start ()
+    {
+        Debug.Log("Start()");
+        smClient = new AsyncTcpSession(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1111));
+        smClient.Connected += SM_Connected;
+        smClient.DataReceived += SMt_DataReceived;
+        smClient.Error += SM_Error;
+        smClient.Closed += SM_Closed;
+        smClient.Connect();
+    }
+
+    private void SM_Closed(object sender, EventArgs e)
+    {
+        Debug.Log("SM_Closed()");
+        if (smClient != null)
+        {
+            if (smClient.IsConnected)
+            {
+                smClient.Close();
+            }
+            smClient = null;
+        }
+    }
+
+    private void SM_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+    {
+        Debug.Log(e.Exception.Message.ToString());
+    }
+
+    private void SMt_DataReceived(object sender, DataEventArgs cmd)
+    {
+        Debug.Log("SMt_DataReceived()");
+        var len = BitConverter.ToUInt16(cmd.Data, 0);
+        var cmdid = BitConverter.ToUInt16(cmd.Data, 2);
+        if (cmdid ==(int)Const.CMD.SM2C_GET_LOGINGATE_REPLY)
+        {
+            byte[] body = new byte[len - 2];
+            Buffer.BlockCopy(cmd.Data, 4, body, 0, len - 2);
+
+            var data = Serializer.Deserialize<CmdMessage.RePly_Get_LoginGateInfo>(new MemoryStream(body));
+            if(data.error==(int)Const.ERROR_CODE.ERROR_OK)
+            {
+                Debug.Log(string.Format("error:{0} ip:{1} port:{2}", data.error, data.ip, data.port));
+            }
+            else
+            {
+                Debug.Log("没有可用网关!");
+            }
+
+        }
+    }
+
+    private void SM_Connected(object sender, EventArgs e)
+    {
+        Debug.Log("SM_Connected()");
+        SendCmd(smClient, Const.CMD.C2SM_GET_LOGINGATE, new byte[] { });
+    }
+
+    void Update () 
+    {
+        Debug.Log("Update()");
+    }
+
+    public void OnLoginClick()
+    {
+        Debug.Log("登录成功");
+    }
+    public void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+        if (smClient != null)
+        {
+            if (smClient.IsConnected)
+            {
+                smClient.Close();
+            }
+            smClient = null;
+        }
+    }
+}
